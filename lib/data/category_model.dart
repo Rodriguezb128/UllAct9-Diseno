a52e53b86e70c298c444300f1b3f962b39ad4bbd
BLANCA_VALERIA_RODRIGUEZ_CHAVEZ <a.21308051280403@cbtis128.edu.mx>
2024-05-08 16:15:43 +0000

    actualizado

---
+++ lib/data/category_model.dart
@@ -0,0 +1,14 @@
+class Category {
+  final int id;
+  final String name;
+
+  Category(this.id, this.name);
+}
+
+List<Category> categories = [
+  Category(0, 'All'),
+  Category(1, 'Skate'),
+  Category(2, 'Basket'),
+  Category(3, 'Raqueta'),
+  Category(4, 'Box'),
+];
---
+++ lib/data/plant_data.dart
@@ -0,0 +1,70 @@
+import 'package:rodriguez/data/plant_model.dart';
+
+List<Plants> plants = [
+  Plants(
+    id: 0,
+    name: 'Skaterboard',
+    imagePath: 'assets/images/image1.jpg',
+    category: 'Deporte',
+    description: 'Una Patineta color azul con un diseño de calavera ',
+    price: 75.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 1,
+    name: 'Balon de Basket',
+    imagePath: 'assets/images/image2.jpg',
+    category: 'Balon de basket de color girs y negro',
+    description: 'Balon de basket color girs y negro ',
+    price: 75.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Balon de Futboll ',
+    imagePath: 'assets/images/image3.jpg',
+    category: 'Balon',
+    description: 'Balon de Futboll color blanco con amarillo naranja ',
+    price: 60.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 3,
+    name: 'Raqueta de tenis',
+    imagePath: 'assets/images/image5.jpg',
+    category: 'Deporte',
+    description: 'Raqueta de tenis con su pelota de tenis',
+    price: 90.00,
+    isFavorit: false,
+  ),
+];
+
+List<Plants> populerPlants = [
+  Plants(
+    id: 0,
+    name: 'Basket',
+    imagePath: 'assets/images/01.jpg',
+    category: 'Balon',
+    description: 'Balon de Basket color naranja',
+    price: 75.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 1,
+    name: 'Futboll',
+    imagePath: 'assets/images/02.jpg',
+    category: 'Balon',
+    description: 'Balon de futboll comun ',
+    price: 75.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Basketboll',
+    imagePath: 'assets/images/03.jpg',
+    category: 'Balon',
+    description: 'Balon de Basket color dorado',
+    price: 60.00,
+    isFavorit: false,
+  ),
+];
